531148834d250ad7d00cfe2079d96fc3c8546352
Abhinav Jha <jhaab@us.ibm.com>
2016-11-15 19:24:13 -0500

    Removed secrets to get started

--- app.js
+++ app.js
@@ -2,4 +2,26 @@ var passport = require('passport');
 var passport_local = require('passport-local');
 var PPH = require('./polypasswordhasher.js');
 
+var secrets = require('./secrets');
+
+var THRESHOLD = 10;
+
+var pph = new PPH(THRESHOLD, 'securepasswords');
+
+var secrets = secrets.secrets;
+
+try{
+    pph.unlock_password_data(secrets);
+}
+catch(e) {
+    console.log("Less number of shares than threshold");
+}
+
+if(pph.is_valid_login("alice", "kitten")){
+    console.log("Valid");
+}
+else{
+    console.log("Invalid");
+}
+
 console.log('Works');
