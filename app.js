206e806b534eefd573076b3d75fbc4a26a2f4156
Abhinav Jha <jhaab@us.ibm.com>
2016-11-22 14:02:14 -0500

    Adding ejs views

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var passport = require('passport');
 var PPHStrategy = require('./lib').Strategy;
 var secrets = require('./secrets').secrets;
+var bodyParser = require('body-parser');
 
 passport.use(new PPHStrategy(
     {"threshold": 10, "filename": "securepasswords", "secrets": secrets}
@@ -9,29 +10,47 @@ passport.use(new PPHStrategy(
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(passport.initialize());
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
+    res.render('login', { messages: ''});
+});
 
+app.get('/login', function(req, res) {
+    res.render('login', { messages: ''});
+});
+
+app.post('/login', function(req, res) {
+
+    /*
     var req = {
         "username" : "alice",
         "password" : "kitten"
     };
 
+    */
+
     passport.authenticate('pph', function(err, user, info){
-        if(err)
-            throw err;
+        if(err){
+            console.log('No such user');
+        }
 
         if(!user){
-            res.send("Invalid Username/Password");
+            res.render('login', { messages: "Invalid password"});
         }
         else{
-            res.send(user);
+            res.send("Welcome " + user);
+            //res.send(user);
         }
             
-    })(req); 
+    })(req.body); 
+});
 
-
+app.get('/register', function(req, res) {
+    res.render('register');
 });
 
 app.listen(3000, function() {
